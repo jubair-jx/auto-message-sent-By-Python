10c3968c5e2271f383f664cd30338ade4afc1199
Mohammad Jubair <you@example.com>
2022-11-12 20:39:43 +0600

    1st

---
+++ autoreply.py
@@ -0,0 +1,8 @@
+import pyautogui
+import time
+times = 100
+while times>0:
+    time.sleep(1)
+    pyautogui.typewrite('Boddaa Kalkee Campus ee ashbii niii.?')
+    time.sleep(1)
+    pyautogui.press('Enter')
