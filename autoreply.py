7e335619c0c227bb48fd79e07334405e2e7f5fbe
Mohammad Jubair <88099075+Jubair360@users.noreply.github.com>
2022-11-12 20:54:00 +0600

    Update autoreply.py

--- autoreply.py
+++ autoreply.py
@@ -3,6 +3,6 @@ import time
 times = 100
 while times>0:
     time.sleep(1)
-    pyautogui.typewrite('Boddaa Kalkee Campus ee ashbii niii.?')
+    pyautogui.typewrite('I love Programmig. I love this one')
     time.sleep(1)
     pyautogui.press('Enter')
